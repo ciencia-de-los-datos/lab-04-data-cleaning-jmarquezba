58341f4ba4ba49e2fbb24b55cd17dc4919229c85
jmarquezba <124936850+jmarquezba@users.noreply.github.com>
2024-04-30 14:09:30 -0500

    solucion del lab4

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,44 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
+from datetime import datetime
+import re
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    file = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    df = file.copy()
+    
+    df = df.dropna()
+    
+    df["sexo"] = df["sexo"].str.lower()
 
-    #
-    # Inserte su código aquí
-    #
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+
+    df["idea_negocio"] = df["idea_negocio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+
+    df["barrio"] = df["barrio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+
+    df["estrato"] = df["estrato"].astype(int)
+
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+
+    df["fecha_de_beneficio"] = [
+        (
+            datetime.strptime(date, "%d/%m/%Y")
+            if bool(re.search(r"\d{1,2}/\d{2}/\d{4}", date))
+            else datetime.strptime(date, "%Y/%m/%d")
+        )
+
+        for date in df["fecha_de_beneficio"]
+        ]
+    
+    df["monto_del_credito"] = df["monto_del_credito"].str.strip("$").str.replace(r"\.00", "", regex=True).str.replace(",","").astype(int)
+    df["línea_credito"] = df["línea_credito"].str.strip().str.lower().str.replace("-", " ").str.replace("_", " ").str.replace(". ", ".")
+    
+    df = df.drop_duplicates()
 
     return df
+
+
